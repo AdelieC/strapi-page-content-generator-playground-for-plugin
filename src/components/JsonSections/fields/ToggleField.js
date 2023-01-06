5221c69109c1325350edff5e5fb1d6190a756774
AdelieC <40043440+AdelieC@users.noreply.github.com>
2023-01-06 16:19:01 +0100

    fixed boolean/string error on togglefield

--- src/components/JsonSections/fields/ToggleField.js
+++ src/components/JsonSections/fields/ToggleField.js
@@ -12,29 +12,33 @@ const ToggleField = ({ name, baseTradId }) => {
         <ToggleInput
           hint={formatMessage({
             id: getTrad(`${baseTradId}.hint`),
-            defaultMessage: "Your text...",
+            defaultMessage: "Your text..."
           })}
           error={
             meta.error &&
             formatMessage({
               id: getTrad(`${baseTradId}.error`),
-              defaultMessage: "Please enter a valid text.",
+              defaultMessage: "Please enter a valid text."
             })
           }
           label={formatMessage({
             id: getTrad(`${baseTradId}.label`),
-            defaultMessage: baseTradId,
+            defaultMessage: baseTradId
           })}
           name={name}
           onLabel={formatMessage({
             id: getTrad(`${baseTradId}.on-label`),
-            defaultMessage: "Yes",
+            defaultMessage: "Yes"
           })}
           offLabel={formatMessage({
             id: getTrad(`${baseTradId}.off-label`),
-            defaultMessage: "No",
+            defaultMessage: "No"
           })}
-          checked={field.value}
+          checked={
+            typeof field.value === "boolean"
+              ? field.value
+              : field.value === "true"
+          }
           onChange={(e) => form.setFieldValue(name, e.target.checked)}
         />
       )}
